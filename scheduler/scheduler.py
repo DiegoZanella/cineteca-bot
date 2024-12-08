1fdcf8e836887b47020bcb55d9fcbc30b98d8c05
DiegoZanella <diegozanella@ciencias.unam.mx>
2024-12-07 18:47:50 -0600

    Changed logging logic for scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -2,6 +2,16 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 import datetime
 import os
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Log level
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
+    handlers=[
+        logging.StreamHandler()  # Output logs to stdout for Docker
+    ]
+)
 
 # SCRAPER_URL = "http://172.17.0.1:5001/scraper"
 # BOT_URL = "http://bot_service_container:5000/send"
@@ -10,18 +20,18 @@ SCRAPER_URL = os.getenv("SCRAPER_URL")
 BOT_URL = os.getenv("BOT_URL")
 RUN_HOUR = int(os.getenv("RUN_HOUR", 7))  # Default to 7 AM
 RUN_MINUTE = int(os.getenv("RUN_MINUTE", 0))  # Default to 0 minutes
-print(RUN_HOUR)
-print(RUN_MINUTE)
+logging.info(RUN_HOUR)
+logging.info(RUN_MINUTE)
 
 
 def run_daily_task():
     # Step 1: Trigger the scraper to scrape today's data
     today = datetime.date.today().strftime("%Y-%m-%d")
-    print(f"Triggering the scraper for date: {today}")
+    logging.info(f"Triggering the scraper for date: {today}")
     scraper_response = requests.get(f"{SCRAPER_URL}?date={today}")
 
     if scraper_response.status_code == 200:
-        print("Scraper completed successfully.")
+        logging.info("Scraper completed successfully.")
         # Step 2: Notify the bot to send the data
         #bot_response = requests.post(BOT_URL)
         #if bot_response.status_code == 200:
@@ -29,7 +39,7 @@ def run_daily_task():
         #else:
         #    print(f"Bot failed: {bot_response.text}")
     else:
-        print(f"Scraper failed: {scraper_response.text}")
+        logging.info(f"Scraper failed: {scraper_response.text}")
 
 
 # Configure the scheduler
@@ -37,5 +47,5 @@ scheduler = BlockingScheduler()
 scheduler.add_job(run_daily_task, 'cron', hour=RUN_HOUR, minute=RUN_MINUTE)  # Run at 7:00 AM
 
 if __name__ == "__main__":
-    print("Starting scheduler...")
+    logging.info("Starting scheduler...")
     scheduler.start()
