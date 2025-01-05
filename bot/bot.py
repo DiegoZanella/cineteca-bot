e563366ccd5b97c98a5bfd0a3a80604061e82516
DiegoZanella <diegozanella@ciencias.unam.mx>
2025-01-05 16:19:48 -0600

    added logging for request_scrapping function

--- bot/bot.py
+++ bot/bot.py
@@ -46,7 +46,7 @@ def send_welcome(message):
 @bot.message_handler(commands=["scrape"])
 def request_scrapping(message):
     date = message.text.split(" ")[0]
-    logging.info(str(message.text.split("")))
+    logging.info(str(message.text.split(" ")))
     logging.info(message.text)
     if len(date) == 0:
         date = datetime.date.today().strftime("%Y-%m-%d")
